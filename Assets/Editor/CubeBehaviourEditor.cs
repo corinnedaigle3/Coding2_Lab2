13c1cb4a45eb97eeec555285958858524ec0e7c1
LilyW3865 <li733414@ucf.edu>
2025-09-03 20:39:54 -0400

    Comments

--- Assets/Editor/CubeBehaviourEditor.cs
+++ Assets/Editor/CubeBehaviourEditor.cs
@@ -10,16 +10,20 @@ public class CubeBehaviourEditor : Editor
         base.OnInspectorGUI();
 
         EditorGUILayout.BeginHorizontal();
+
+        //Creates button to select all the cubes
         if (GUILayout.Button("Select all cubes"))
         {
             var allCubeBehaviour = GameObject.FindObjectsOfType<CubeBehaviour>();
             var allCubeGameObjects = allCubeBehaviour.Select(cube => cube.gameObject).ToArray();
             Selection.objects = allCubeGameObjects;
         }
+        //Creates button to clear the selection 
         if (GUILayout.Button("Clear selection"))
         {
             Selection.objects = new Object[] {(target as CubeBehaviour).gameObject};
         }
+
         EditorGUILayout.EndHorizontal();
 
         var cachedColor = GUI.backgroundColor;
@@ -27,8 +31,10 @@ public class CubeBehaviourEditor : Editor
 
         using (new EditorGUILayout.HorizontalScope())
         {
+            //Creates button to disable/enable all the cubes
             if (GUILayout.Button("Disable/Enable all cubes", GUILayout.Height(40)))
             {
+                //Loop that controls the actual enabling/disabling of the game objects
                 foreach (var cube in GameObject.FindObjectsOfType<CubeBehaviour>(true))
                 {
                     Undo.RecordObject(cube.gameObject, "Disable/Enable cube");
--- Assets/Editor/SphereBehaviourEditor.cs
+++ Assets/Editor/SphereBehaviourEditor.cs
@@ -10,16 +10,20 @@ public class SphereBehaviourEditor : Editor
         base.OnInspectorGUI();
 
         EditorGUILayout.BeginHorizontal();
+
+        //Creates button to select all the spheres
         if (GUILayout.Button("Select all spheres"))
         {
             var allSphereBehaviour = GameObject.FindObjectsOfType<SphereBehaviour>();
             var allSphereGameObjects = allSphereBehaviour.Select(sphere => sphere.gameObject).ToArray();
             Selection.objects = allSphereGameObjects;
         }
+        //Creates button to clear the selection 
         if (GUILayout.Button("Clear selection"))
         {
             Selection.objects = new Object[] {(target as SphereBehaviour).gameObject};
         }
+        
         EditorGUILayout.EndHorizontal();
 
         var cachedColor = GUI.backgroundColor;
@@ -27,8 +31,10 @@ public class SphereBehaviourEditor : Editor
 
         using (new EditorGUILayout.HorizontalScope())
         {
+            //Creates button to disable/enable all the spheres
             if (GUILayout.Button("Disable/Enable all spheres", GUILayout.Height(40)))
             {
+                //Loop that controls the actual enabling/disabling of the game objects
                 foreach (var sphere in GameObject.FindObjectsOfType<SphereBehaviour>(true))
                 {
                     Undo.RecordObject(sphere.gameObject, "Disable/Enable cube");
